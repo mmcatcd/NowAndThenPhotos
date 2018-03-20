aedc0c7833e9b044d01de86a3d2ecd57ebf5cf07
Michael McAndrew <michael@corribdigital.com>
2018-03-20 12:25:52 +0000

    New Scene added location option and temporary keyboard avoid fix

--- PhotosProject/components/NewScene.js
+++ PhotosProject/components/NewScene.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import {View, Text, StyleSheet} from 'react-native';
-import {ImagePicker} from 'expo';
+import {View, Text, StyleSheet, Alert, TextInput, Modal, Platform, KeyboardAvoidingView, Keyboard} from 'react-native';
+import {ImagePicker, Location, Permissions, MapView, Constants} from 'expo';
 import SceneButton from './SceneButton';
 import SourceButton from './SourceButton';
+import Dimensions from 'Dimensions';
+import NewPhoto from './NewPhoto';
 import t from 'tcomb-form-native';
 
 const Form = t.form.Form;
+
+const window = Dimensions.get('window');
 
 //Redux imports
 import {bindActionCreators} from 'redux';
@@ -18,27 +22,39 @@ const FormType = t.struct({
     name: t.String
 });
 
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#fff", 
-        flex: 1,
-        justifyContent: 'center',
-    },
-    formContainer: {
-        marginRight: 20, 
-        marginLeft: 20, 
-        marginBottom: 40, 
-    },
-    buttonContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row'
-    }
-});
+let id = null;
+
+const GEOLOCATION_OPTIONS = {
+    enableHighAccuracy: true,
+    timeout: 20000,
+    maximumAge: 1000,
+    timeInterval: 1,
+    distanceInterval: 1
+ };
 
 class NewScene extends React.Component {
     state = {
-        image: null
+        image: null,
+        name: null,
+        nameFocus: false,
+        cameraVisible: false,
+        location: {
+            latitude: 0,
+            longitude: 0,
+            altitude: 0,
+            latitudeDelta: 0.0922, 
+            longitudeDelta: 0.0421
+        },
+        sceneLocation: null,
+        errorMessage: null,
+        searchText: null,
+        locationFrom: 'GPS',
+        locationSearch: null,
+        keyboard: {
+            show: false,
+            height: 0,
+            textFocused: false
+        },
     }
 
     static navigationOptions = {
@@ -46,19 +62,159 @@ class NewScene extends React.Component {
         headerRight: (<View />),
     };
 
-    componentDidMount() {
-        this.refs.form.getComponent('name').refs.input.focus();
+    removeLocationWatcher = null;
+
+    componentWillMount() {
+        if (Platform.OS === 'android' && !Constants.isDevice) {
+            this.setState({
+              errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+            });
+          } else {
+            this._getLocationAsync();
+        }
+
+        this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this._keyboardDidShow.bind(this));
+        this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide.bind(this));
     }
 
-    handleSubmit() {
-        this.props.createScene(this.refs.form.getValue().name);
-        this.props.navigation.navigate('Home');
+    _getLocationAsync = async() => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({
+                errorMessage: 'Permission to access location was denied',
+            });
+            console.log('Not granted!');
+        }
+
+        await Location.watchPositionAsync(GEOLOCATION_OPTIONS, (newLoc) => {
+            if(this.state.locationFrom == 'GPS') {
+                this.setState({
+                    location: {
+                        latitude: newLoc.coords.latitude,
+                        longitude: newLoc.coords.longitude,
+                        altitude: newLoc.coords.altitude
+                    }
+                });
+            }
+        }).then(watcher => this.removeLocationWatcher = watcher.remove);
+    }
+
+    _attemptGeocodeAsync = async () => {
+        this.setState({locationFrom: 'USER'});
+        try {
+            if(this.state.searchText != null) {
+                let locationSearch = await Location.geocodeAsync(this.state.searchText);
+                this.setState({
+                    location: {
+                        latitude: locationSearch[0].latitude,
+                        longitude: locationSearch[0].longitude,
+                        altitude: locationSearch[0].altitude
+                    }
+                });
+            }
+        } catch(e) {
+            Alert.alert(
+                'Search Failed',
+                `Couldn't find any results`,
+                [
+                    {text: 'OK'}
+                ]
+            )
+        }
+    }
+
+    componentWillUnmount() {
+        this.removeLocationWatcher && this.removeLocationWatcher();
+
+        this.keyboardDidShowListener.remove();
+        this.keyboardDidHideListener.remove();
+    }
+
+    _keyboardDidShow(e) {
+        this.setState({
+            keyboard: {
+                ...this.state.keyboard,
+                show: true,
+                height: e.endCoordinates.height,
+            }
+        });
+    }
+
+    focusLocationText() {
+        this.setState({
+            keyboard: {
+                ...this.state.keyboard,
+                textFocused: true
+            }
+        });
+    }
+
+    unFocusLocationText() {
+        this.setState({
+            keyboard: {
+                ...this.state.keyboard,
+                textFocused: false
+            }
+        });
+        
+    }
+
+    _keyboardDidHide() {
+        this.setState({keyboard:{show: false, height: 0}});
+    }
+
+    async handleSubmit() {
+        const name = this.state.name;
+        const image = this.state.image;
+        const location = this.state.location;
+
+        let containsScene = Object.values(this.props.scenes).find(scene => {
+            return scene.name == name;
+        })
+
+        if(containsScene == null && image != null && name != null && location != null) {
+            await this.props.createScene(name);
+
+            id = Object.values(this.props.scenes).find(scene => {
+                return scene.name == name;
+            });
+
+            id = Object.values(id)[0];
+
+            await this.props.createPhoto(image, id);
+            await this.props.addLocation(location, id);
+
+            this.props.navigation.navigate('SceneView', {sceneId: id});
+        } else if (image == null) {
+            Alert.alert(
+                'No Image Selected',
+                'Select an image and try again.',
+                [
+                    {text: 'OK'}
+                ]
+            )
+        } else if(name == null) {
+            Alert.alert(
+                `Invalid Title`,
+                `You didn't add a title for the scene.`,
+                [
+                    {text: 'OK'}
+                ]
+            )
+        } else {
+            Alert.alert(
+                'Invalid Title',
+                'Title is already taken. Try again',
+                [
+                    {text: 'OK'}
+                ]
+            )
+        }
     }
 
     pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-            allowsEditing: true,
-            aspect: [4, 3],
+            allowsEditing: true
         });
 
         this.setState({
@@ -66,17 +222,83 @@ class NewScene extends React.Component {
         });
     };
 
+    closeModal() {
+        this.setState({cameraVisible: false});
+    }
+
+    updateImage(image, location) {
+        this.setState({image: image, location: location});
+        console.log(this.state.location);
+    }
+
     render() {
+        const location = this.state.location;
+        const keyboard = this.state.keyboard;
+
+        if(location.latitude == 0)
+            return <View />
+
         return(
-            <View style={styles.container}>
-                {/*<Header title="new scene" />*/}
+            <View style={[styles.container, {marginTop: keyboard.textFocused ? -keyboard.height : 0}]}>
+                <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.cameraVisible}
+                    onRequestClose={this.closeModal.bind(this)}>
+                    <NewPhoto onExit={this.closeModal.bind(this)} imageUpdate={this.updateImage.bind(this)} />
+                </Modal>
                 <View style={styles.container}>
-                    <View style={styles.formContainer}><Form ref="form" type={FormType} /></View>
-                    <View style={styles.buttonContainer}>
-                        <SourceButton text="Take Photo" icon="camera" color="#F93943" onPress={() => this.props.navigation.navigate('SceneCamera')} />
-                        <SourceButton text="Camera Roll" icon="image" color="#F93943" onPress={this.pickImage} />
+                    <View style={styles.formContainer}>
+                        <TextInput
+                            style={styles.textInput}
+                            onChangeText={name => this.setState({name})}
+                            underlineColorAndroid="transparent"
+                            selectionColor="#f93943"
+                            value={this.state.text}
+                            maxLength={30}
+                            onFocus={() => this.setState({nameFocus: true})}
+                            onBlur={() => this.setState({nameFocus: false})}
+                            placeholder="Scene Name" />
                     </View>
-                    <View style={{marginTop: 50, alignItems: 'center'}}>
+                    <View style={styles.buttonContainer}>
+                        {/*<SourceButton text="Take Photo" icon="camera" color="#F93943" onPress={() => this.props.navigation.navigate('SceneCamera', {})} />*/}
+                        <SourceButton text="Take Photo" icon="camera" color="#F93943" onPress={() => this.setState({cameraVisible: true})} />
+                        <SourceButton text="Camera Roll" icon="image" backgroundColor="#F93943" color="#fff" onPress={this.pickImage} />
+                    </View>
+                    <View style={styles.mapContainer}>
+                        <MapView
+                            style={styles.map}
+                            showsMyLocationButton={true}
+                            showsUserLocation={true}
+                            region={{
+                                latitude: location.latitude,
+                                longitude: location.longitude,
+                                latitudeDelta: 0.0922, 
+                                longitudeDelta: 0.0421
+                            }}
+                        >
+                            <MapView.Marker 
+                                coordinate={{
+                                    latitude: location.latitude,
+                                    longitude: location.longitude
+                                }} 
+                            />
+                        </MapView>
+                        <View style={[styles.formContainer, {flex: 1}]}>
+                            <TextInput
+                                style={styles.textInput}
+                                onChangeText={text => this.setState({searchText: text})}
+                                underlineColorAndroid="transparent"
+                                selectionColor="#f93943"
+                                value={this.state.searchText}
+                                placeholder="Current Location"
+                                onSubmitEditing={this._attemptGeocodeAsync}
+                                onFocus={this.focusLocationText.bind(this)}
+                                onBlur={this.unFocusLocationText.bind(this)}
+                                />
+                        </View>
+                    </View>
+                    <View style={{flex: 1, alignItems: 'center'}}>
                         <SceneButton text="Create Scene" color="#f93943" width={150} topPad={5} onPress={this.handleSubmit.bind(this)} />
                     </View>
                 </View>
@@ -84,6 +306,42 @@ class NewScene extends React.Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#fff", 
+        flex: 1,
+        justifyContent: 'center'
+    },
+    formContainer: {
+        marginRight: 40, 
+        marginLeft: 40, 
+        marginBottom: 40, 
+        marginTop: 10
+    },
+    buttonContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginBottom: 20,
+    },
+    mapContainer: {
+        flex: 2,
+        marginTop: 20,
+        marginBottom: 20
+    },
+    map: {
+        //flex: 3,
+        paddingTop: 20,
+        height: window.height/5
+    },
+    textInput: {
+        height: 40, 
+        borderBottomColor: '#f93943', 
+        borderBottomWidth: 2
+    }
+});
 
 const mapStateToProps = (state, props) => ({ loading: state.sceneReducer.loading, photos: state.sceneReducer.photos, scenes: state.sceneReducer.scenes })
 const mapDispatchToProps = (dispatch) => bindActionCreators(Actions, dispatch)
